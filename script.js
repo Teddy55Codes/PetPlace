8bdb10d382ebbba5a99f554b6bb8fca63886e0ff
Teddy55Codes <teddy@teddy.email>
2023-04-09 22:36:32 +0200

    rectangle now follows the cursor

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const CatMovementIntervalInMs = 50
+const CatStopsPxFromTheCursor = 100
+
+var XCord = 0;
+var YCord = 0;
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+
 (function() {
     document.onmousemove = handleMouseMove;
     function handleMouseMove(event) {
@@ -20,7 +29,41 @@
                 (doc && doc.clientTop  || body && body.clientTop  || 0 );
         }
 
-        let positionText = document.getElementById("position")
-        positionText.innerText = event.pageX + "/" + event.pageY
+        let positionText = document.getElementById("position");
+        positionText.innerText = event.pageX + "/" + event.pageY;
+        XCord = event.pageX;
+        YCord = event.pageY;
     }
 })();
+
+function CreateACat() {
+    const div = document.createElement("div");
+
+    div.className = "cat";
+    div.style = "background-color: red;";
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.position = 'absolute';
+    document.body.appendChild(div);
+}
+
+(async function() {
+    while (true) {
+        for (let catDiv of document.getElementsByClassName("cat")) {
+            let rect = catDiv.getBoundingClientRect();
+            if (XCord > rect.x && XCord - rect.x >= CatStopsPxFromTheCursor) {
+                catDiv.style.left = (rect.x + 1) + 'px';
+            } else if (XCord < rect.x && rect.x - XCord >= CatStopsPxFromTheCursor) {
+                catDiv.style.left = (rect.x - 1) + 'px';
+            }
+
+            if (YCord > rect.y && YCord - rect.y >= CatStopsPxFromTheCursor) {
+                catDiv.style.top = (rect.y + 1) + 'px';
+            } else if (YCord < rect.y && rect.y - YCord >= CatStopsPxFromTheCursor) {
+                catDiv.style.top = (rect.y - 1) + 'px';
+            }
+        }
+        await delay(CatMovementIntervalInMs);
+    }
+})();
+
